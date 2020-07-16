a2187cd86689ef2106c8462d762053c66ea0ca99
yokn <discolouredattic@gmail.com>
2020-07-16 14:25:52 +0300

    expand plan

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-# Classes:
-#   Player
-#   Board
-#   Game
-#   ?Ai?
+# Plan:
+#   ::Player:
+#     ::Human
+#     ::Ai
+#       #select_random_pegs:
+#         @code array
+#   ::Board:
+#       @guesses hash (Turn: pegs)
+#       #is_full?
+#   ::Game:
+#       #setup_game
+#       #determine_gamemaster
+#       #play_game:
+#         get_guess
+#         check_win
+#       #get_guess:
+#         if player:
+#
+#       #check_win
+#         if @guesses.any == @code
+#           win
+#       #game_over?
