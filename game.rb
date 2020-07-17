7535f3145d862a8299be5ad2b9f86ee93e853575
yokn <discolouredattic@gmail.com>
2020-07-17 17:27:58 +0300

    rewrite check_guess

--- game.rb
+++ game.rb
@@ -47,19 +47,42 @@ class Game
   end
 
   def check_guess(guess)
-    @correct_color_and_position = 0
-    @wrong_position = 0
+    @flag = []
+    @perfect_match = 0
+    @color_match = 0
     @ai_player.code.each_with_index do |value, index|
-      if value == guess[index]
-        @correct_color_and_position += 1
-      elsif guess.include?(value)
-        @wrong_position += 1
+      check_color_match(value, guess, index) unless check_perfect_match(value, guess, index)
+      # remove_duplicate_matches
+    end
+    puts "Perfect match: #{@perfect_match}"
+    puts "Color match: #{@color_match}"
+    true if @perfect_match == 4
+  end
+
+  def check_perfect_match(value, guess, index)
+    @found_match = false
+    if guess[index] == value
+      @perfect_match += 1
+      puts "Found perfect match: #{value}"
+      @flag[index] = 1
+      @found_match = true
+    end
+    @found_match
+  end
+
+  # https://stackoverflow.com/a/2005808
+  def check_color_match(value, guess, index)
+    guess.each_with_index do |inner_value, inner_index|
+      if inner_index != index && inner_value == value && !@flag[inner_index]
+        @color_match += 1
+        @flag[inner_index] = 1
       end
     end
-    puts "Correct color and position: #{@correct_color_and_position}"
-    puts "Wrong position: #{@wrong_position}"
-    true if @correct_color_and_position == 4
   end
+
+  # def remove_duplicate_matches
+  #   @@abc = (@perfect_match > @color_match ? @color_match : @perfect_match) - @perfect_match
+  # end
 
   def game_over?
     check_guess(@guess) || @board.full? unless @guess.nil?
